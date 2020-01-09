bd6db1750015f0aecd0c5e7d2acec49764a29c53
varinp <varinp@pc-dg-037-12.iut2.upmf-grenoble.fr>
2020-01-09 09:18:17 +0100

    Nom du fichier

---
+++ src/ProjetIle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author varinp
+ */
+public class ProjetIle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+        System.out.println("Hell");
+        int nb=0;
+       System.out.println("Hello");
+    } ;;;;
+}
