97eaa181458325639bc14c588587d414a32f14e4
varinp <varinp@pc-dg-037-12.iut2.upmf-grenoble.fr>
2020-01-09 15:43:23 +0100

    Début de l'IHM

---
+++ src/Jeu/IHM.java
@@ -0,0 +1,29 @@
+package Jeu;
+
+
+import vuesIHM.*;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author varinp
+ */
+public class IHM {
+    private VueInscription inscription;
+    private VueIntroduction introduction;
+    private VueJeu jeu;
+    
+
+    IHM() {
+        //test des vues
+      this.inscription = new VueInscription(this);
+      this.introduction = new VueIntroduction(this);
+    }
+    
+}
