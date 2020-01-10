9265361b10f977dbccb1fb875cff2bb241271978
varinp <varinp@pc-dg-037-12.iut2.upmf-grenoble.fr>
2020-01-10 09:21:46 +0100

    Implémention classe

--- src/vuesIHM/VueJoueur.java
+++ src/vuesIHM/VueJoueur.java
@@ -2,7 +2,9 @@ package vuesIHM;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /*
@@ -17,15 +19,29 @@ import javax.swing.JPanel;
  */
 public class VueJoueur {
     private VueJeu jeu = new VueJeu();
+    private JFrame window = new JFrame("Vue Joueur");
+    private JPanel contenu = new JPanel(new BorderLayout());
+    private JPanel contenuHaut = new JPanel(new GridLayout(2, 1));
+    private JPanel contenuMillieu = new JPanel(new GridLayout(4, 1));
+    private JPanel contenuBas = new JPanel(new GridLayout(2, 3));
+    
+    private JLabel nomJoueur = new JLabel("NomJoueur");
+    private JLabel nomRole = new JLabel("nomRole");
+    private JLabel nbactions = new JLabel("nbactions");
+    private JLabel listeTresors = new JLabel("Trésors");
+    private JLabel listeCartes = new JLabel("listeCartes");
+    
+    
+    private JButton deplacer = new JButton("Se déplacer");
+    private JButton assecher = new JButton("Assécher");
+    private JButton donner = new JButton("Donner");
+    private JButton prendre = new JButton("Prendre");
+    private JButton utiliserPouvoir = new JButton("");
+    
 
     public VueJoueur() {
-        JFrame window = new JFrame("Vue Joueur");
-        JPanel contenu = new JPanel(new BorderLayout());
-        JPanel contenuHaut = new JPanel(new GridLayout(2, 1));
-        JPanel contenuMillieu = new JPanel(new GridLayout(4, 1));
-        JPanel contenuBas = new JPanel(new GridLayout(2, 3));
-        
         //Configuraton de contenuHaut
+        
         
         //Configuration de contenuMillieu
         
