89d58eeea573e6061a2763c65e667d96e2f3c308
varinp <varinp@pc-dg-037-12.iut2.upmf-grenoble.fr>
2020-01-10 09:06:27 +0100

    Implémention classe

--- src/vuesIHM/VueJoueur.java
+++ src/vuesIHM/VueJoueur.java
@@ -1,5 +1,10 @@
 package vuesIHM;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +16,26 @@ package vuesIHM;
  * @author varinp
  */
 public class VueJoueur {
+    private VueJeu jeu = new VueJeu();
+
+    public VueJoueur() {
+        JFrame window = new JFrame("Vue Joueur");
+        JPanel contenu = new JPanel(new BorderLayout());
+        JPanel contenuHaut = new JPanel(new GridLayout(2, 1));
+        JPanel contenuMillieu = new JPanel(new GridLayout(4, 1));
+        JPanel contenuBas = new JPanel(new GridLayout(2, 3));
+        
+        //Configuraton de contenuHaut
+        
+        //Configuration de contenuMillieu
+        
+        //Configuration de contenuBas
+        
+        //Configuration window
+        window.setSize(500,500);
+        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+        
+    }
+    
     
 }
