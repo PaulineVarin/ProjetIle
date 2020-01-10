3f852476b35157fbf035a551d385b2b777057fe9
tardieue <tardieue@pc-dg-037-05.iut2.upmf-grenoble.fr>
2020-01-10 14:59:53 +0100

    j'ai créer lesd classes

--- src/Ile/IleInterdite.java
+++ src/Ile/IleInterdite.java
@@ -17,10 +17,13 @@ import java.util.ArrayList;
 public class IleInterdite extends Observe {
     
     private int niveauEau;
+    private ArrayList <String> tresorsRecuperes = new ArrayList<>();
     private ArrayList <CarteTirage> carteTiragesTires = new ArrayList <>();
     private ArrayList <CarteTirage> carteTiragesDefausses = new ArrayList <>();
+    private ArrayList <CarteInondation> carteInondesTires = new ArrayList <>();
     private ArrayList <CarteInondation> carteInondesDefausses = new ArrayList <>();
-    
+    private Grille grille = null;
+    private Aventurier aventurier = null;
     
     
     
