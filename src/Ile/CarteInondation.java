6d2ecce445b74b54e7c94ed514f70b32a5b7ce2a
balananw <balananw@pc-dg-037-08.iut2.upmf-grenoble.fr>
2020-01-10 14:44:48 +0100

    Réécriture de CarteInondation

---
+++ src/Ile/CarteInondation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ile;
+
+/**
+ *
+ * @author tardieue
+ */
+public class CarteInondation {
+    private String nomTuile;
+    
+    public CarteInondation(String nomTuile) {
+        this.nomTuile = nomTuile;
+    }
+}
