a0151d22bb5a81c7ffd4c7ddd26e23a72a84a422
varinp <varinp@pc-dg-037-12.iut2.upmf-grenoble.fr>
2020-01-10 11:13:20 +0100

    Remplissage classes IHM

--- src/vuesIHM/VueResume.java
+++ src/vuesIHM/VueResume.java
@@ -20,6 +20,33 @@ public class VueResume {
     
     private JFrame window = new JFrame("VueRésumé");
     private JPanel contenu = new JPanel(new BorderLayout());
+    private JPanel contenuCentre = new JPanel(new GridLayout(3, 1));
+    
+    private JLabel libel = new JLabel("Ile Interdite");
+    private JLabel libeljoueurcourant = new JLabel("Joueur courant");
+    private JLabel libelNiveauEau = new JLabel("NiveauEau");
+    private JLabel libelTirage = new JLabel("Tirage Cartes");
+
+    public VueResume(/*ajouter element vueJeu*/) {
+        //Configuration contenuCentre
+        contenuCentre.add(libeljoueurcourant);
+        contenuCentre.add(libelNiveauEau);
+        contenuCentre.add(libelTirage);
+        
+        //Configuration contenu
+        contenu.add(libel, BorderLayout.NORTH);
+        contenu.add(contenuCentre,BorderLayout.CENTER);
+        
+        //Configuration fenetre
+        window.setSize(500,500);
+        window.add(contenu);
+        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE); //A supprimer lors de la mise en place
+        window.setVisible(true);
+    }
+    
+    
+    
+    
     
     
 }
