5ca0fcb3ea2bd114b1baaf21a5f3af5be33f1f13
varinp <varinp@transit.iut2.upmf-grenoble.fr>
2020-01-13 10:28:52 +0100

    Classe Message

--- src/patterns/observateur/Message.java
+++ src/patterns/observateur/Message.java
@@ -5,12 +5,46 @@
  */
 package patterns.observateur;
 
+import Enumeration.*;
+import Ile.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  *
  * @author tardieue
  */
 public class Message implements Serializable {
+    //Donnees
+    private TypeMessage typeM;
+    private TypeAction typeA;
+    private Aventurier a;
+    private int niveauEau;
+    private int nbJoueurs;
+    private int nbActions;
+    private int nbActionsDonneur;
+    private String nomRole;
+    private String nomCarte;
+    private String nomRoleDonneur;
+    private String nomRoleReceveur;
+    private String nomTuile;
+    private TypeTresorTuile typeTresor;
+    private ArrayList<CarteTirage> collectCartesSpe;
+    private ArrayList<CarteTirage> collectCartesTresor;
+    private ArrayList<CarteTirage> collectCartesJoueur;
+    private ArrayList<Aventurier> collectJoueurs;
+    private ArrayList<Tuile> collectTuiles;
+    private ArrayList<String> collectNomsJoueurs;
+   
+    
+    //Constructeur
+    public Message(TypeMessage typeM) {
+        this.typeM = typeM;
+    }
+    
+    
+    //Méthodes
+    
+  
     
 }
