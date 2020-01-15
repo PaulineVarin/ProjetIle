93232dc09e74f76306b2a66f5f9bb4f57ff177f9
balananw <balananw@pc-dg-025-05.iut2.upmf-grenoble.fr>
2020-01-15 12:12:35 +0100

    Correction de getCollectJoueurs dans Grille

--- src/Ile/Grille.java
+++ src/Ile/Grille.java
@@ -143,7 +143,7 @@ public class Grille {
     
     public ArrayList<Aventurier> getCollectJoueurs(Tuile t) {
         ArrayList<Aventurier> aventuriers = new ArrayList<>();
-        aventuriers = t.getCollectAventuriers();
+        aventuriers = t.getJoueurs();
         return aventuriers;
     }
     
